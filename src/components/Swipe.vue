c8b56720d4ed3c58383ac8f1b726c4a89a4c454d
Goh Wei Wen <goweiwen@gmail.com>
2017-10-16 00:58:00 +0800

    Add faces to Swipe page

--- src/components/Swipe.vue
+++ src/components/Swipe.vue
@@ -1,20 +1,20 @@
 <template>
   <!-- if you want automatic padding use "layout-padding" class -->
-  <div class="layout-padding justify-center is-centered">
-    <vue-swing>
-      <q-card class="card">
+  <div class="layout-padding">
+    <vue-swing @throwout="throwout" class="row justify-center">
+      <q-card class="card" v-for="person in persons" key="person.name">
         <q-card-media>
-          <img src="~assets/quasar-logo-full.svg">
+          <img class="person-picture no-select" :src="person.picture">
         </q-card-media>
         <q-card-title>
-          Card Title
+          {{ person.name }}
         </q-card-title>
         <q-card-separator />
         <q-card-main>
           Card Content
         </q-card-main>
         <q-card-actions align="around">
-          <q-btn flat round small color="red"><q-icon name="favorite" /></q-btn>
+          <q-btn flat round small color="red" @click="throwout"><q-icon name="favorite" /></q-btn>
           <q-btn flat round small color="faded"><q-icon name="bookmark" /></q-btn>
           <q-btn flat round small color="primary"><q-icon name="share" /></q-btn>
         </q-card-actions>
@@ -50,7 +50,30 @@ export default {
   },
 
   data () {
-    return {}
+    return {
+      persons: []
+    }
+  },
+
+  mounted () {
+    this.fetchPerson()
+  },
+
+  methods: {
+    throwout () {
+      this.persons.pop()
+      this.fetchPerson()
+    },
+    async fetchPerson () {
+      const res = await fetch('https://randomuser.me/api/?inc=gender,name,picture')
+      const data = await res.json()
+      const person = data.results[0]
+      this.persons.push({
+        picture: person.picture.large,
+        name: `${person.name.first} ${person.name.last}`,
+        gender: person.gender === 'male' ? 1 : 0
+      })
+    }
   }
 }
 </script>
@@ -58,4 +81,11 @@ export default {
 <style lang="stylus" scoped>
 .card
   max-width 400px
+
+.no-select
+  user-select none
+  pointer-events none
+
+.person-picture
+  width 80vw
 </style>
