e448885d48be95bd4d8324be19f11d4f1bbbc698
Goh Wei Wen <goweiwen@gmail.com>
2017-10-18 12:41:15 +0800

    Change API url for production

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,3 @@
-export const API_URL = 'http://localhost:3000/api'
+export const API_URL = process.env.NODE_ENV === 'production'
+  ? 'https://pear.c0t5zumpiujt.ap-southeast-1.rds.amazonaws.com/api'
+  : 'http://localhost:3000/api'
