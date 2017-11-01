938650bb998d71910a1bccff91100079d417857f
Goh Wei Wen <goweiwen@gmail.com>
2017-11-01 19:21:53 +0800

    Revert "Add user's name to toolbar"
    This reverts commit 0e67a6aefd1831cbe3b1b5b37b06c8b9d188435f.

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,46 +1,32 @@
 <template>
   <!-- Configure "view" prop for QLayout -->
   <q-layout ref="layout" view="lHr LpR lfr" class="layout" :class="{ matchmaker: isMatchmakerMode, dater: !isMatchmakerMode }">
-    <div slot="header">
-      <q-toolbar slot="header" class="text-tertiary bg-secondary">
-        <q-btn flat class="hide-on-drawer-visible" @click="$refs.layout.toggleLeft()">
-          <img src="~assets/overflow.png" width="24" height="24">
-        </q-btn>
+    <q-toolbar slot="header" class="text-tertiary bg-secondary">
+      <q-btn flat class="hide-on-drawer-visible" @click="$refs.layout.toggleLeft()">
+        <img v-if="me != null" class="user-photo" :src="`https://graph.facebook.com/${me.facebookId}/picture?type=large`" width="32" height="32">
+      </q-btn>
 
-        <q-toolbar-title>
-          <img class="banner" src="~assets/banner.png" width="86" height="40">
-          <img src="~assets/arrow-down.png" width="12" height="12" style="margin:12px 4px">
+      <q-toolbar-title>
+        <img class="banner" src="~assets/banner.png" width="86" height="40">
+        <img src="~assets/arrow-down.png" width="12" height="12" style="margin:12px 4px">
 
-          <q-popover ref="popover" anchor="bottom middle" self="top middle">
-            <q-list item-separator link>
-              <q-item @click="$store.dispatch('setMatchmakerMode', { isMatchmakerMode: true }), $router.push('/'),  $refs.popover.close()">
-                Matchmaker
-              </q-item>
-              <q-item @click="$store.dispatch('setMatchmakerMode', { isMatchmakerMode: false }), $router.push('/'),  $refs.popover.close()">
-                Dater
-              </q-item>
-            </q-list>
-          </q-popover>
-        </q-toolbar-title>
+        <q-popover ref="popover" anchor="bottom middle" self="top middle">
+          <q-list item-separator link>
+            <q-item @click="$store.dispatch('setMatchmakerMode', { isMatchmakerMode: true }), $refs.popover.close()">
+              Matchmaker
+            </q-item>
+            <q-item @click="$store.dispatch('setMatchmakerMode', { isMatchmakerMode: false }), $refs.popover.close()">
+              Dater
+            </q-item>
+          </q-list>
+        </q-popover>
+      </q-toolbar-title>
 
-        <div style="width:38px;margin-right:0.2rem"></div>
-      </q-toolbar>
-
-      <q-toolbar slot="header" class="text-tertiary bg-secondary">
-        <q-toolbar-title v-if="isMatchmakerMode && user != null && user.facebookId != null" class="user">
-          <img class="photo" :src="`https://graph.facebook.com/${user.facebookId}/picture?type=large`" width="32" height="32">
-          <span class="name">{{ user.facebookName }}</span>
-        </q-toolbar-title>
-        <q-toolbar-title v-else-if="me != null && me.facebookId != null" class="user">
-          <img class="photo" :src="`https://graph.facebook.com/${me.facebookId}/picture?type=large`" width="32" height="32">
-          <span class="name">{{ me.facebookName }}</span>
-        </q-toolbar-title>
-      </q-toolbar>
-
-    </div>
+      <div style="width:38px;margin-right:0.2rem"></div>
+    </q-toolbar>
 
     <div slot="left">
-      <sidebar @close="$refs.layout.hideLeft()" />
+      <sidebar slot="left" @close="$refs.layout.hideLeft()" />
     </div>
 
     <router-view v-if="doneInitialFetch" />
@@ -69,15 +55,11 @@ export default {
   data() {
     return {
       doneInitialFetch: false,
-      id: null,
     }
   },
 
   computed: mapState({
     me: ({ users, me }) => users[me],
-    user({ users }) {
-      return users[this.id]
-    },
     isMatchmakerMode: ({ isMatchmakerMode }) => isMatchmakerMode,
   }),
 
@@ -92,27 +74,16 @@ export default {
   mounted() {
     this.$store.dispatch('fetchMe').then(() => (this.doneInitialFetch = true))
   },
-
-  watch: {
-    $route(route) {
-      this.id = route.params.id
-    },
-  },
 }
 </script>
 
 <style lang="stylus" scoped>
 @import '../themes/app.variables'
 
-.user
-  font-weight 500
+.user-photo
+  margin-left -2px
+  border-radius 100%
 
-  .photo
-    vertical-align middle
-    margin-right 2px
-    border-radius 100%
-
-.q-toolbar
   transition-delay 0.25s
 
 .layout
