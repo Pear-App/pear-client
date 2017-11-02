cba2162a7155387cfad2f9791420c30293d780de
Ana Chua <anaprimawaty@gmail.com>
2017-11-03 02:20:41 +0800

    Add local push notification

--- src/util/cordova.js
+++ src/util/cordova.js
@@ -1,4 +1,4 @@
-// import router from '../router'
+import router from '../router'
 
 document.addEventListener('deviceready', () => {
   // Facebook
@@ -7,11 +7,32 @@ document.addEventListener('deviceready', () => {
     global.FB.login = global.FB.login.papp(['public_profile', 'user_photos'])
   }
 
+  // Push Notifications
   if (typeof FCMPlugin !== 'undefined') {
     window.FCMPlugin.getToken(function(token) {
       global.fcmToken = token
     })
+
+    window.FCMPlugin.onNotification(function(data) {
+      if (data.wasTapped) {
+        // app on background and push notification tapped
+        router.push(data.route)
+      } else {
+        // app on foreground, trigger local push notification
+        cordova.plugins.notification.local.schedule({
+          title: data.title,
+          text: data.text,
+          foreground: true,
+          data: { route: data.route },
+        })
+      }
+    })
   }
+
+  cordova.plugins.notification.local.on('click', function(notification) {
+    // app on foreground and push notification tapped
+    router.push(notification.data.route)
+  })
 
   // Universal Links
   /* global universalLinks */
