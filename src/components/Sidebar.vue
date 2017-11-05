8de41753b9b2fe5af58ca1bc9811ae252704b2bb
Si Kai <sikai.ngsk@gmail.com>
2017-11-05 13:50:10 +0800

    hide own profile if not single

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,7 +2,7 @@
   <loader v-if="me == null || singles == null || friends == null"/>
   <q-scroll-area v-else>
     <q-list no-border link inset-delimiter>
-      <q-side-link item :key="me.id" replace :to="`/user/${me.id}/swipe`">
+      <q-side-link v-if="me.isSingle" item :key="me.id" replace :to="`/user/${me.id}/swipe`">
         <q-item-side :avatar="`https://graph.facebook.com/${me.facebookId}/picture?type=large`" />
         <q-item-main>
           {{ me.facebookName }}
