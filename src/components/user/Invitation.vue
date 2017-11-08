175b1fe84fc188712efc4ee260ae555ac3f3bc06
Goh Wei Wen <goweiwen@gmail.com>
2017-11-08 14:04:49 +0800

    Style invitation and join pages

--- src/components/user/Invitation.vue
+++ src/components/user/Invitation.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="layout-padding">
-    <div class="profile">
-      <span class="name">{{ user.nickname }}, {{ user.age }}</span>
-      <span class="school">{{ user.school }}</span>
-      <span class="major">{{ user.major }}</span>
-    </div>
+  <div class="layout-padding invitaiton text-tertiary text-center">
     <div>
-      <q-btn color="primary" @click="shareInvitationLink">Share</q-btn>
+      <img class="pear" src="~assets/pear.png">
+      <p>
+        <big class="title">Invite {{ user.nickname }} to join Pear!</big>
+      </p>
+      <p>
+        <span>For {{ whom }} to indicate {{ whose }} dating prefernces before your curation begins.</span>
+      </p>
+      <q-btn big color="primary" @click="shareInvitationLink">Invite</q-btn>
     </div>
   </div>
 </template>
@@ -19,15 +21,27 @@ export default {
 
   props: ['id'],
 
+  data() {
+    return {
+      open: true,
+    }
+  },
+
   computed: {
     user() {
       return this.$store.state.users[this.id]
+    },
+    whom() {
+      return this.user.sex == 'M' ? 'him' : 'her'
+    },
+    whose() {
+      return this.user.sex == 'M' ? 'his' : 'her'
     },
   },
 
   methods: {
     async shareInvitationLink() {
-      const url = `https://pear.netlify.com/join/${this.user.id}`
+      const url = `https://pear.netlify.com/#/join/${this.user.id}`
       if (navigator.share) {
         try {
           navigator.share({
@@ -54,5 +68,16 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-@import '../../themes/app.variables'
+.invitation
+  padding 15%
+  display flex
+  justify-content center
+  align-items center
+  height calc(100vh - 106px)
+
+  .pear
+    margin-bottom 1em
+
+  .title
+    font-weight 500
 </style>
