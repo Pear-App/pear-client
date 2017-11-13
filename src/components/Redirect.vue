41ee2f66e70f995b17a6d7a49cab82213d2e8a65
Goh Wei Wen <goweiwen@gmail.com>
2017-11-13 21:36:06 +0800

    Fix route when logout

--- src/components/Redirect.vue
+++ src/components/Redirect.vue
@@ -11,5 +11,17 @@ export default {
       this.$router.replace('/invite/1')
     }
   },
+
+  beforeRouteEnter() {
+    if (store.state.friends.length > 0) {
+      this.$router.replace(`/user/${store.state.me}/swipe`)
+    } else if (store.state.singles.length > 0) {
+      this.$router.replace(`/user/${store.state.singles[0]}/swipe`)
+    } else if (store.state.invitations.length > 0) {
+      this.$router.replace(`/user/${store.state.invitations[0]}/profile`)
+    } else {
+      this.$router.replace('/invite/1')
+    }
+  },
 }
 </script>
